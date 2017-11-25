6b1b075723da87cf5c14d5ef53b42c74cf06e9e4
Alex Lipov <tech.alexl@gmail.com>
2017-11-25 12:17:54 +0200

    First working version commit. Currently supports dropbox assignment only with one attached file.

---
+++ src/background.js
@@ -0,0 +1,191 @@
+console.log('in background.js');
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+	function(details) {
+		console.log('tab updated ' + details.url);
+		chrome.tabs.sendMessage(details.tabId, {
+			event: 'url_changed',
+			url: details.url
+		});
+	});
+
+/* poor man's test */
+function isTextContentType(contentType) {
+	if (contentType == null) {
+		return false;
+	}
+	if (contentType.startsWith('text')) {
+		return true;
+	}
+	switch (contentType) {
+		case 'application/json':
+		case 'application/xml':
+			return true;
+	}
+	return false;
+}
+
+function loadAssignmentUrl(baseUrl, assignment, student, callback) {
+	var path = "/teacher_dropbox_assignment/grade/" + assignment.id + "?student=" + student.id;
+	var url = baseUrl + path;
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('GET', url, true);
+
+	xhttp.onload = function() {
+		if (xhttp.status !== 200) {
+			console.log("Couldn't load student page " + url + "; status=" + xhttp.status);
+			callback({
+				succeeded: false,
+				student: student
+			});
+			return;
+		}
+
+		// let's try to find the uploaded file url
+		var parser = new DOMParser();
+		var doc = parser.parseFromString(xhttp.responseText, "text/html");
+		var links = doc.getElementsByTagName("a");
+		var result = null;
+		for (var i = 0; i < links.length; i++) {
+			var ref = links[i].getAttribute('href');
+			if (ref != null && ref.startsWith('/files/' + student.id)) {
+				console.log('found! ' + ref);
+				result = ref;
+				break;
+			}			
+		}
+
+		if (result == null) {
+			console.log("Failed to find file url in: " + url);
+			callback({
+				succeeded: false,
+				student: student
+			});
+			return;
+		}
+		var assignmentUrl = baseUrl + result;
+		console.log("assignmentUrl for student " + student.id + " is: " + assignmentUrl);
+
+		callback({
+			succeeded: true,
+			student: student,
+			assignmentUrl: assignmentUrl
+		});
+	}
+
+	xhttp.onerror = function() {
+		callback({
+			succeeded: false,
+			student: student
+		});
+	}
+
+	xhttp.send();
+}
+
+function downloadAssignment(assignmentUrl, student, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', assignmentUrl, true);
+	xhr.responseType = 'blob';
+
+	xhr.onload = function(e) {
+		if (xhr.status !== 200) {
+			console.log("Couldn't download attachment for " + assignmentUrl + "; status=" + xhr.status);
+			callback({
+				succeeded: false
+			});
+			return;
+		}
+
+		var filename = assignmentUrl.substring(assignmentUrl.lastIndexOf('/') + 1);
+		var contentType = xhr.getResponseHeader('content-type');
+		var isBinary = !isTextContentType(contentType);
+
+		console.log("downloaded attachment for: " + assignmentUrl);
+
+		callback({
+			succeeded: true,
+			isBinary: isBinary,
+			filename: student.name + '_' + filename,
+			data: xhr.response
+		});
+	}
+
+	xhr.onerror = function() {
+	    callback({
+			succeeded: false
+		});
+	}
+
+	xhr.send();
+}
+
+function createAssignmentUrlPromise(baseUrl, assignment, student) {
+	return new Promise(function(resolve, reject) {
+		loadAssignmentUrl(baseUrl, assignment, student, function(result) {
+			resolve(result);
+		});
+	});
+}
+
+function createDownloadAssignmentPromise(assignmentUrl, student, totalItems, tabId) {
+	return new Promise(function(resolve, reject) {
+		downloadAssignment(assignmentUrl, student, function(result) {
+			chrome.tabs.sendMessage(tabId, {
+				event: 'attachment_downloaded',
+				itemsLength: totalItems
+			});
+			resolve(result);
+		});
+	});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+	if (request.action == 'download-all') {
+
+		var assignmentUrlPromises = [];
+		request.data.forEach(function(student) {
+			var promise = createAssignmentUrlPromise(request.baseUrl, request.assignment, student);
+			assignmentUrlPromises.push(promise);
+		});
+
+		Promise.all(assignmentUrlPromises).then(function(results) {
+			console.log("all assignmentUrl promises completed!");
+
+			var assignmentDownloadPromises = [];
+			results.forEach(function(result) {
+				if (!result.succeeded) {
+					return;
+				}
+				var downloadPromise = createDownloadAssignmentPromise(result.assignmentUrl, result.student, request.data.length, sender.tab.id);
+				assignmentDownloadPromises.push(downloadPromise);
+			});
+			
+			Promise.all(assignmentDownloadPromises).then(function(results) {
+				console.log("all assignmentDownload promises completed!");
+
+				var zip = new JSZip();
+				results.forEach(function(result) {
+					if (!result.succeeded) {
+						return;
+					}
+					zip.file(result.filename, result.data, { binary: result.isBinary });
+				});
+
+				zip.generateAsync({ type: "blob" }).then(function(content) {
+					callback({
+						finished: true
+					});
+					var url = URL.createObjectURL(content);
+					chrome.downloads.download({
+						url: url,
+						filename: request.assignment.name + '.zip'
+					});
+					// saveAs(content, "some.zip");
+				});
+			});
+		});
+
+		return true; // prevents the callback from being called too early on return
+	}
+});
---
+++ src/content.js
@@ -0,0 +1,140 @@
+// alert("loaded!");
+// console.log('in content.js'); 
+
+function getLastPathElement(path) {
+	let lastIndex = path.lastIndexOf('/');
+	return lastIndex == -1 ? path : path.substr(lastIndex + 1);
+}
+
+function replaceAll(str, search, replacement) {
+	// seems like this gives best performance on Chrome (see comments at https://stackoverflow.com/a/1145525/1233652)
+	return str.split(search).join(replacement); 
+}
+
+var progressCounter = 0;
+
+function initialize() {
+	var intervalObj = setInterval(function() {
+		var tableForms = $(".tableForm");
+		if (tableForms.length > 0) {
+			clearInterval(intervalObj);
+
+			if (tableForms.length > 1) {
+				// shouldn't happen
+				console.log("found too many tableForms");
+				return;
+			}
+
+			// console.log('tableForm found');
+			var tableForm = tableForms[0];
+			var studentData = [];
+
+			$(tableForm).find('a').each(function() {
+			    var title = $(this).attr('title');
+			    if (title == undefined) {
+			        return;
+			    }
+			    var href = $(this).attr('href');
+			    var userId = getLastPathElement(href);
+			    studentData.push({
+			    	name: replaceAll(title, ', ', '_'),
+			    	id: userId
+			    });
+			});
+
+   			// best guess to find assignment's name
+   			var nameCandidates = [];
+   			$("body").find('h2').each(function() {
+   				var id = $(this).attr('id');
+   				if (id != undefined) {
+   					return;
+   				}
+   				var text = $(this).text();
+   				if (text === 'Grades') {
+   					return;
+   				}
+   				nameCandidates.push(text);
+   			});
+   			var assignmentName = nameCandidates.length != 1 ? '' : replaceAll(nameCandidates[0], ' ', '_');
+
+			var downloadAllButtons = $("#download-all-button");
+			if (downloadAllButtons.length == 0) {
+				var injected = $('<button id="download-all-button" class="button1" name="commit" type="submit">Download All Answers</button>');
+				$(injected).insertBefore(tableForm);
+				$('<br/>').insertBefore(injected);
+
+				$(injected).click(function(e) {
+					$(e.target).html("Downloading..");
+					$(e.target).prop("disabled", true);
+
+					var locationString = window.location.toString();
+					var lastElement = getLastPathElement(locationString); locationString.substr(locationString.lastIndexOf('/') + 1);
+
+					progressCounter = 0;
+
+					chrome.runtime.sendMessage({
+						action: 'download-all',
+						assignment: {
+							id: lastElement,
+							name: assignmentName
+						},
+						data: studentData,
+						baseUrl: window.location.origin
+					}, function(result) {
+						if (result.finished) {
+							$(e.target).html("Downloaded!");
+							var prevBackgroundColor = $(this).css("background-color");
+							$(this).css("background-color","green");
+							var prevBorderColor = $(this).css("border-color");
+							$(this).css("border-color","green");
+							var prevColor = $(this).css("color");
+							$(this).css("color","white");
+
+							setTimeout(function() {
+								$(this).html("Download All Answers");
+								$(this).css("background-color", prevBackgroundColor);
+								$(this).css("border-color", prevBorderColor);
+								$(this).css("color", prevColor);
+								$(this).removeAttr('disabled');
+							}.bind(this), 2000);
+						}
+					}.bind(this));
+				});
+			}
+		} else {
+			console.log('tableForm not found');
+		}
+	}, 100);
+}
+
+(function() {
+    (function waitForCompletion() {
+        if(document.readyState == "complete") {
+        	initialize();
+        } else {
+            setTimeout(waitForCompletion, 300);
+            // console.log('not in complete state');
+        }
+    })();
+})();
+
+chrome.runtime.onMessage.addListener(
+	function(message, sender, sendResponse) {
+		if (message != null) {
+			// console.log('received message: ' + message.event);
+			switch (message.event) {
+				case 'url_changed':
+					// console.log('url updated: ' + message.url);
+					initialize();
+					break;
+				case 'attachment_downloaded':
+					progressCounter++;
+					if (progressCounter == message.itemsLength) {
+						$("#download-all-button").html("Zipping..");
+					} else {
+						$("#download-all-button").html("Downloading [" + progressCounter + "/" + message.itemsLength + "]..");
+					}
+					break;
+			}
+		}
+	});
